00d5eeb26ae9559d72859d2397bbeb192b725653
bosung park <81157873+bosungpark@users.noreply.github.com>
2023-03-04 17:32:03 +0900

    Add cmd in basic cmd

--- basic_cmd.py
+++ basic_cmd.py
@@ -1,6 +1,4 @@
 """
-pre-setting
-
 xcode-select --install
 brew install openssl xz zlib gdbm sqlite
 
@@ -9,5 +7,8 @@ CPPFLAGS="-I$(brew --prefix zlib)/include"
 LDFLAGS="-L$(brew --prefix zlib)/lib"   
 ./configure --with-openssl=$(brew --prefix openssl)
 
-make -j2 -s  
+make -j2 -s  -> this cmd conpiles file, or simply use "make" without options
+./python.exe -> this is python bin file
+
+make altinstall -> this cmd make usable my independent version of python or "sudo make altinstall"
 """

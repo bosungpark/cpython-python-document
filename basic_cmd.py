593bf5ccfc512954957e5b55e791235537f329e3
bosung park <81157873+bosungpark@users.noreply.github.com>
2023-03-04 17:33:09 +0900

    Update basic_cmd.py

--- basic_cmd.py
+++ basic_cmd.py
@@ -7,7 +7,7 @@ CPPFLAGS="-I$(brew --prefix zlib)/include"
 LDFLAGS="-L$(brew --prefix zlib)/lib"   
 ./configure --with-openssl=$(brew --prefix openssl)
 
-make -j2 -s  -> this cmd conpiles file, or simply use "make" without options
+make -j2 -s  -> this cmd compiles file, or simply use "make" without options
 ./python.exe -> this is python bin file
 
 make altinstall -> this cmd make usable my independent version of python or "sudo make altinstall"
